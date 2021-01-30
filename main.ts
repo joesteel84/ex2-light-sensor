25951ba4cac1fbf0cbfe7d49a19258a437bd5237
joesteel84 <78255885+joesteel84@users.noreply.github.com>
2021-01-30 09:56:42 +0000

    changed from 200 to 100

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 let light2 = 0
 basic.forever(function () {
     light2 = pins.analogReadPin(AnalogPin.P0)
-    if (light2 > 200) {
+    if (light2 > 100) {
         basic.showLeds(`
             # . # . #
             . # # # .
